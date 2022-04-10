ea148add8dca12369ebdefa52d1bf82370837702
yakupozbaydar <yakup.ozbaydarft@gmail.com>
2022-04-10 15:57:59 +0300

    toplamaislemi
    toplama işlemi metodu yazıldı ve test edildi

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String [] args){
+        char islem;
+        int birinci,ikinci,sonuc;
+
+        Scanner giris = new Scanner (System.in);
+
+        System.out.println("işlem seçini: + ");
+        islem = giris.next().charAt(0);
+        System.out.println("ilk sayıyı girin:");
+
+        birinci = giris.nextInt();
+
+        System.out.println("ikinci sayıyı girin:");
+
+        ikinci = giris.nextInt();
+
+        switch (islem) {
+            case '+':
+                sonuc=birinci+ikinci;
+                System.out.println("sonuç:"+ sonuc);
+                break;
+            default:
+                System.out.println("Invalid operator!");
+                break;
+        }
+    giris.close();
+    }
+}
